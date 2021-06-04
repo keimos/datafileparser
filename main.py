8290ede7c61bf22662a0fc78d7734f3a4458c9f7
keimos <keithmosleyjr@gmail.com>
2021-06-04 13:54:36 -0400

    Added exception handling

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ import pandas as pd
 result = pd.read_csv('DeliveryReservationFailureFrom_05_27.csv', usecols=['Request'])
 
 print(result)  # prints results of data read from CSV
+
+a = [1, 2, 3]
+try:
+    print("Second element = %d" %(a[1]))
+
+    print("Fourth element = %d" %(a[3]))
+
+except IndexError:
+    print("An error occurred")
+
